e0f799f1a3bbd08be7e036168e24fc34c3b8dcba
tsung77777 <10946022@ntub.edu.tw>
2023-05-26 16:39:03 +0800

    fix bug

--- 112406_backend/src/main/kotlin/com/example/responses/TaskResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.example.responses
-//用來回傳json轉出成data class
-class TaskResponse(
-    val id: Int,
-    val account: String,
-    val password: String,
-    val exp: String
-)
--- 112406_backend/src/main/kotlin/com/example/responses/TimesResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.responses
-//用來回傳json轉出成data class
-class TimesResponse(
-    val id: Int,
-    val fitnessdate: String,
-    val pushup: String,
-    val sqart: String,
-    val plank: String
-)
