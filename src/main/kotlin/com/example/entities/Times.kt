093433e7e40d3c57c36826fb10766e61d1e8acc4
tsung77777 <120070698+tsung77777@users.noreply.github.com>
2023-05-28 18:13:26 +0800

    Update Times.kt

--- src/main/kotlin/com/example/entities/Times.kt
+++ src/main/kotlin/com/example/entities/Times.kt
@@ -2,13 +2,17 @@ package com.example.entities
 
 import com.example.tables.*
 import org.jetbrains.exposed.dao.*
-import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.*
 import org.joda.time.*
 //代表一筆資料的class
 class Times(id: EntityID<Int>) : IntEntity(id){
     companion object: IntEntityClass<Times>(Timess)//這筆資料來源是Tasks
     var fitnessdate: DateTime by Timess.fitnessdate
-    var pushup:String by Timess.pushup
-    var sqart:String by Timess.sqart
-    var plank:String by Timess.plank
+    var pushup:Int by Timess.pushup
+    var sqart:Int by Timess.sqart
+    var plank:Int by Timess.plank
+    var leg_lift:Int by Timess.leg_lift
+    var lunge_squat:Int by Timess.lunge_squat
+    var bike_crunch:Int by Timess.bike_crunch
+    var bridge:Int by Timess.bridge
 }
