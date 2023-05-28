c0c3a98193cd7b9f1f8ee689a03f98219e243612
tsung77777 <120070698+tsung77777@users.noreply.github.com>
2023-05-28 18:12:46 +0800

    Update Timess.kt

--- src/main/kotlin/com/example/tables/Timess.kt
+++ src/main/kotlin/com/example/tables/Timess.kt
@@ -1,14 +1,17 @@
 package com.example.tables
 
-import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.jodatime.datetime
+import org.jetbrains.exposed.dao.id.*
+import org.jetbrains.exposed.sql.jodatime.*
 //用object定義table的欄位和資料型態
 //Tasks : IntIdTable()  繼承id欄位是autoinquiment和prime key
 //複數的object
 object Timess : IntIdTable(){
     val fitnessdate = datetime("fitnessdate")
-    val pushup = varchar("pushup", 25)
-    val sqart= varchar("sqart", 25)
-    val plank = varchar("plank", 25)
-
+    val pushup = integer("pushup").default(0)
+    val sqart= integer("sqart").default(0)
+    val plank = integer("plank").default(0)
+    val leg_lift = integer("leg_lift").default(0)
+    val lunge_squat = integer("lunge_squat").default(0)
+    val bike_crunch = integer("bike_crunch").default(0)
+    val bridge = integer("bridge").default(0)
 }
